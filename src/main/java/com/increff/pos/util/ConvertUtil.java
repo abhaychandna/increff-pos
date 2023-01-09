1939fec68d1994cd7d12eb6878c2678782a0cd49
abhaychandna <abhaychandna5@gmail.com>
2023-01-09 13:44:59 +0530

    Product converts added

--- src/main/java/com/increff/pos/util/ConvertUtil.java
+++ src/main/java/com/increff/pos/util/ConvertUtil.java
@@ -1,10 +1,31 @@
 package com.increff.pos.util;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.increff.pos.model.BrandData;
 import com.increff.pos.model.BrandForm;
+import com.increff.pos.model.ProductData;
+import com.increff.pos.model.ProductForm;
 import com.increff.pos.pojo.BrandPojo;
+import com.increff.pos.pojo.ProductPojo;
+import com.increff.pos.service.ApiException;
+import com.increff.pos.service.BrandService;
 
+@Component
 public class ConvertUtil {
+
+	private static BrandService bService;
+	@Autowired
+	BrandService brandService;
+
+	@PostConstruct
+	private void init(){
+		bService = this.brandService;
+	}
+
     public static BrandData convert(BrandPojo p) {
 		BrandData d = new BrandData();
 		d.setBrand(p.getBrand());
@@ -18,5 +39,28 @@ public class ConvertUtil {
 		p.setBrand(f.getBrand());
 		p.setCategory(f.getCategory());
 		return p;
+	}
+
+
+	public static ProductPojo convert(ProductForm f) throws ApiException {
+		ProductPojo p = new ProductPojo();
+		p.setBarcode(f.getBarcode());
+        BrandPojo brand = bService.getCheckBrandCategory(f.getBrand(), f.getCategory());
+		p.setBrand_category(brand.getId());
+		p.setMrp(f.getMrp());
+		p.setName(f.getName());
+		return p;
+	}
+
+	public static ProductData convert(ProductPojo p) throws ApiException{
+		ProductData f = new ProductData();
+		f.setBarcode(p.getBarcode());
+        BrandPojo brand = bService.get(p.getBrand_category());
+		f.setBrand(brand.getBrand());
+        f.setCategory(brand.getCategory());
+		f.setMrp(p.getMrp());
+		f.setName(p.getName());
+		f.setId(p.getId());
+		return f;
 	}
 }
