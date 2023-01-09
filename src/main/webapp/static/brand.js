110c1f116205d73ba2690101cbd80f124419e513
abhaychandna <abhaychandna5@gmail.com>
2023-01-09 11:03:37 +0530

    brand.html and js added

---
+++ src/main/webapp/static/brand.js
@@ -0,0 +1,220 @@
+
+function getBrandUrl(){
+	var baseUrl = $("meta[name=baseUrl]").attr("content")
+	return baseUrl + "/api/brands";
+}
+
+//BUTTON ACTIONS
+function addBrand(event){
+	//Set the values to update
+	var $form = $("#Brand-form");
+	var json = toJson($form);
+	var url = getBrandUrl();
+
+	$.ajax({
+	   url: url,
+	   type: 'POST',
+	   data: json,
+	   headers: {
+       	'Content-Type': 'application/json'
+       },	   
+	   success: function(response) {
+	   		getBrandList();  
+	   },
+	   error: handleAjaxError
+	});
+
+	return false;
+}
+
+function updateBrand(event){
+	$('#edit-Brand-modal').modal('toggle');
+	//Get the ID
+	var id = $("#Brand-edit-form input[name=id]").val();	
+	var url = getBrandUrl() + "/" + id;
+
+	//Set the values to update
+	var $form = $("#Brand-edit-form");
+	var json = toJson($form);
+
+	$.ajax({
+	   url: url,
+	   type: 'PUT',
+	   data: json,
+	   headers: {
+       	'Content-Type': 'application/json'
+       },	   
+	   success: function(response) {
+	   		getBrandList();   
+	   },
+	   error: handleAjaxError
+	});
+
+	return false;
+}
+
+
+function getBrandList(){
+	var url = getBrandUrl();
+	$.ajax({
+	   url: url,
+	   type: 'GET',
+	   success: function(data) {
+	   		displayBrandList(data);  
+	   },
+	   error: handleAjaxError
+	});
+}
+
+function deleteBrand(id){
+	var url = getBrandUrl() + "/" + id;
+
+	$.ajax({
+	   url: url,
+	   type: 'DELETE',
+	   success: function(data) {
+	   		getBrandList();  
+	   },
+	   error: handleAjaxError
+	});
+}
+
+// FILE UPLOAD METHODS
+var fileData = [];
+var errorData = [];
+var processCount = 0;
+
+
+function processData(){
+	var file = $('#BrandFile')[0].files[0];
+	readFileData(file, readFileDataCallback);
+}
+
+function readFileDataCallback(results){
+	fileData = results.data;
+	uploadRows();
+}
+
+function uploadRows(){
+	//Update progress
+	updateUploadDialog();
+	//If everything processed then return
+	if(processCount==fileData.length){
+		getBrandList();
+		return;
+	}
+	
+	//Process next row
+	var row = fileData[processCount];
+	processCount++;
+	
+	var json = JSON.stringify(row);
+	var url = getBrandUrl();
+
+	//Make ajax call
+	$.ajax({
+	   url: url,
+	   type: 'POST',
+	   data: json,
+	   headers: {
+       	'Content-Type': 'application/json'
+       },	   
+	   success: function(response) {
+	   		uploadRows();  
+	   },
+	   error: function(response){
+	   		row.error=response.responseText
+	   		errorData.push(row);
+	   		uploadRows();
+	   }
+	});
+
+}
+
+function downloadErrors(){
+	writeFileData(errorData);
+}
+
+//UI DISPLAY METHODS
+
+function displayBrandList(data){
+	var $tbody = $('#Brand-table').find('tbody');
+	$tbody.empty();
+	for(var i in data){
+		var e = data[i];
+		
+		var buttonHtml = ' <button onclick="displayEditBrand(' + e.id + ')">edit</button>'
+		var row = '<tr>'
+		+ '<td>' + e.id + '</td>'
+		+ '<td>' + e.brand + '</td>'
+		+ '<td>'  + e.category + '</td>'
+		+ '<td>' + buttonHtml + '</td>'
+		+ '</tr>';
+        $tbody.append(row);
+	}
+}
+
+function displayEditBrand(id){
+	var url = getBrandUrl() + "/" + id;
+	$.ajax({
+	   url: url,
+	   type: 'GET',
+	   success: function(data) {
+	   		displayBrand(data);   
+	   },
+	   error: handleAjaxError
+	});	
+}
+
+function resetUploadDialog(){
+	//Reset file name
+	var $file = $('#BrandFile');
+	$file.val('');
+	$('#BrandFileName').html("Choose File");
+	//Reset various counts
+	processCount = 0;
+	fileData = [];
+	errorData = [];
+	//Update counts	
+	updateUploadDialog();
+}
+
+function updateUploadDialog(){
+	$('#rowCount').html("" + fileData.length);
+	$('#processCount').html("" + processCount);
+	$('#errorCount').html("" + errorData.length);
+}
+
+function updateFileName(){
+	var $file = $('#BrandFile');
+	var fileName = $file.val();
+	$('#BrandFileName').html(fileName);
+}
+
+function displayUploadData(){
+ 	resetUploadDialog(); 	
+	$('#upload-Brand-modal').modal('toggle');
+}
+
+function displayBrand(data){
+	$("#Brand-edit-form input[name=name]").val(data.brand);	
+	$("#Brand-edit-form input[name=age]").val(data.category);	
+	$("#Brand-edit-form input[name=id]").val(data.id);	
+	$('#edit-Brand-modal').modal('toggle');
+}
+
+
+//INITIALIZATION CODE
+function init(){
+	$('#add-Brand').click(addBrand);
+	$('#update-Brand').click(updateBrand);
+	$('#refresh-data').click(getBrandList);
+	$('#upload-data').click(displayUploadData);
+	$('#process-data').click(processData);
+	$('#download-errors').click(downloadErrors);
+    $('#BrandFile').on('change', updateFileName)
+}
+
+$(document).ready(init);
+$(document).ready(getBrandList);
+
