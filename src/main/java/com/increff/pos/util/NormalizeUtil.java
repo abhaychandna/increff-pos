cbd8a0f4d0565a7a1c57385b4838a9414a13196c
abhaychandna <abhaychandna5@gmail.com>
2023-01-09 14:01:15 +0530

    NormalizeProduct added

--- src/main/java/com/increff/pos/util/NormalizeUtil.java
+++ src/main/java/com/increff/pos/util/NormalizeUtil.java
@@ -1,10 +1,16 @@
 package com.increff.pos.util;
 
 import com.increff.pos.pojo.BrandPojo;
+import com.increff.pos.pojo.ProductPojo;
 
 public class NormalizeUtil {
     public static void normalize(BrandPojo p) {
 		p.setBrand(StringUtil.toLowerCase(p.getBrand()).trim());
 		p.setCategory(StringUtil.toLowerCase(p.getCategory()).trim());
+	}
+
+	public static void normalize(ProductPojo p){
+		p.setName(p.getName().toLowerCase().trim());
+		p.setBarcode(p.getBarcode().toLowerCase().trim());
 	}
 }
