efe9f17b1a1446c7260abf0cd5caac38f198e76c
abhaychandna <abhaychandna5@gmail.com>
2023-01-09 14:01:30 +0530

    ValidateProduct added

--- src/main/java/com/increff/pos/util/ValidateUtil.java
+++ src/main/java/com/increff/pos/util/ValidateUtil.java
@@ -1,6 +1,9 @@
 package com.increff.pos.util;
 
+import javax.transaction.Transactional;
+
 import com.increff.pos.pojo.BrandPojo;
+import com.increff.pos.pojo.ProductPojo;
 import com.increff.pos.service.ApiException;
 
 public class ValidateUtil {
@@ -10,5 +13,19 @@ public class ValidateUtil {
 			throw new ApiException("Brand cannot be empty");
 		if(StringUtil.isEmpty(p.getCategory()))
 			throw new ApiException("Category cannot be empty");
+	}
+
+    //@Transactional
+	public static void validateProduct(ProductPojo p) throws ApiException{
+		
+		if(StringUtil.isEmpty(p.getBarcode()))
+			throw new ApiException("Barcode cannot be empty");
+		if(StringUtil.isEmpty(p.getName()))
+			throw new ApiException("Name cannot be empty");
+
+		if(p.getMrp()<=0)
+			throw new ApiException("MRP should be positive");	
+		
+		System.out.println("Brand Category " + p.getBrand_category());
 	}
 }
