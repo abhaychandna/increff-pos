64c4eb49198bf5bcf77881a912bea641b58169c5
abhaychandna <abhaychandna5@gmail.com>
2023-01-09 15:13:16 +0530

    refactored all functions to use filter

--- src/main/java/com/increff/pos/dao/BrandDao.java
+++ src/main/java/com/increff/pos/dao/BrandDao.java
@@ -20,29 +20,15 @@ public class BrandDao extends AbstractDao{
 	private EntityManager em;
 	
 	public List<BrandPojo> selectAll() {
-		CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<BrandPojo> cq = cb.createQuery(BrandPojo.class);
-
-        Root<BrandPojo> rootEntry = cq.from(BrandPojo.class);
-
-		CriteriaQuery<BrandPojo> all = cq.select(rootEntry);
-		
-		TypedQuery<BrandPojo> query = em.createQuery(all);
-		return query.getResultList();
+		BrandPojo p = new BrandPojo();
+		return filter(p);
 	}
 
 	public List<BrandPojo> getByBrandCategory(String brand, String category) {
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<BrandPojo> cq = cb.createQuery(BrandPojo.class);
-
-        Root<BrandPojo> BrandPojo = cq.from(BrandPojo.class);
-
-       	Predicate brandPredicate = cb.equal(BrandPojo.get("brand"), brand);
-		Predicate categoryPredicate = cb.equal(BrandPojo.get("category"), category);
-		cq.where(brandPredicate, categoryPredicate);
-
-        TypedQuery<BrandPojo> query = em.createQuery(cq);
-		return query.getResultList();
+		BrandPojo p = new BrandPojo();
+		p.setBrand(brand);
+		p.setCategory(category);
+		return filter(p);
 	}
 
 	public List<BrandPojo> filter(BrandPojo p){
