803cc9cce86548c6694297a59e76c7f302ae6666
abhaychandna <abhaychandna5@gmail.com>
2023-01-09 13:59:32 +0530

    ProductDTO added

---
+++ src/main/java/com/increff/pos/dto/ProductDto.java
@@ -0,0 +1,65 @@
+package com.increff.pos.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.increff.pos.model.ProductData;
+import com.increff.pos.model.ProductForm;
+import com.increff.pos.pojo.BrandPojo;
+import com.increff.pos.pojo.ProductPojo;
+import com.increff.pos.service.ApiException;
+import com.increff.pos.service.BrandService;
+import com.increff.pos.service.ProductService;
+import com.increff.pos.util.StringUtil;
+import com.increff.pos.util.ConvertUtil;
+import com.increff.pos.util.NormalizeUtil;
+import com.increff.pos.util.ValidateUtil;
+
+
+
+@Component
+public class ProductDto {
+    
+    @Autowired
+    ProductService svc;
+    @Autowired
+    BrandService brandService;
+
+    public void add(ProductForm f) throws ApiException {
+        ProductPojo p = ConvertUtil.convert(f);
+        NormalizeUtil.normalize(p);
+		ValidateUtil.validate(p);
+        svc.add(p);
+    }
+
+    public ProductData get(int id) throws ApiException {
+        ProductPojo p = svc.get(id);
+		return ConvertUtil.convert(p);
+    }
+
+    public List<ProductData> getAll() throws ApiException{
+        List<ProductPojo> Products = svc.getAll();
+        List<ProductData> respList = new ArrayList<ProductData>();
+        for (ProductPojo p : Products) {
+            respList.add(ConvertUtil.convert(p));
+        }
+        return respList;
+    }
+
+    public void update(int id, ProductForm f) throws ApiException {
+        ProductPojo p = ConvertUtil.convert(f);
+        NormalizeUtil.normalize(p);
+		ValidateUtil.validate(p);
+        svc.update(id, p);
+    }
+
+
+
+
+
+}
