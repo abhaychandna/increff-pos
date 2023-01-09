62bd1b71e321afb314d5b5e7c3e597bae2080e3e
abhaychandna <abhaychandna5@gmail.com>
2023-01-09 17:19:05 +0530

    create Pojos added

--- src/test/java/com/increff/pos/service/TestUtil.java
+++ src/test/java/com/increff/pos/service/TestUtil.java
@@ -1,8 +1,49 @@
 package com.increff.pos.service;
 
-import com.increff.pos.model.*;
+import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.increff.pos.dto.BrandDto;
+import com.increff.pos.dto.ProductDto;
+import com.increff.pos.model.*;
+import com.increff.pos.pojo.BrandPojo;
+import com.increff.pos.pojo.ProductPojo;
+
+@Component
 public class TestUtil {
+
+    private static BrandDto brandDto;
+    private static ProductDto productDto;
+    @Autowired
+    BrandDto bDto;
+    @Autowired
+    ProductDto pDto;
+
+	@PostConstruct
+	private void init(){
+        brandDto = this.bDto;
+        productDto = this.pDto;
+	}
+
+    public static ProductPojo createProductWithBrand(String barcode, String brand, String category, String name, double mrp) throws ApiException {
+        createBrand(brand, category);
+        ProductPojo p = createProduct(barcode, brand, category, name, mrp);
+        return p;
+    }
+
+    public static BrandPojo createBrand(String brand, String category) throws ApiException{
+        BrandForm bf = TestUtil.getBrandFormDto(brand,category);
+        BrandPojo bp = brandDto.add(bf);
+        return bp;
+    }
+
+    public static ProductPojo createProduct(String barcode, String brand, String category, String name, double mrp) throws ApiException {
+        ProductForm f = TestUtil.getProductFormDto(barcode,brand,category,name,mrp);
+        ProductPojo p = productDto.add(f);
+        return p;
+    }
 
     public static BrandForm getBrandFormDto(String brand, String category) {
         BrandForm brandForm = new BrandForm();
@@ -19,5 +60,12 @@ public class TestUtil {
         productForm.setName(name);
         productForm.setMrp(mrp);
         return productForm;
+    }
+
+    public static InventoryForm getInventoryFormDto(String barcode, int quantity){
+        InventoryForm inventoryForm = new InventoryForm();
+        inventoryForm.setBarcode(barcode);
+        inventoryForm.setQuantity(quantity);
+        return inventoryForm;
     }
 }
