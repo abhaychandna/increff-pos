ce86d0bda02777b24ec3e63703d0267453bbb012
abhaychandna <abhaychandna5@gmail.com>
2023-01-11 17:43:31 +0530

    InventoryTest remove unused imports

--- src/test/java/com/increff/pos/dto/InventoryTest.java
+++ src/test/java/com/increff/pos/dto/InventoryTest.java
@@ -1,34 +1,27 @@
 package com.increff.pos.dto;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-
-import java.util.List;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.increff.pos.model.BrandForm;
 import com.increff.pos.model.InventoryData;
 import com.increff.pos.model.InventoryForm;
 import com.increff.pos.model.ProductData;
-import com.increff.pos.pojo.BrandPojo;
-import com.increff.pos.pojo.InventoryPojo;
-import com.increff.pos.pojo.ProductPojo;
 import com.increff.pos.service.AbstractUnitTest;
 import com.increff.pos.service.ApiException;
 import com.increff.pos.service.TestUtil;
 
-public class InventoryTest extends AbstractUnitTest{
-    
+public class InventoryTest extends AbstractUnitTest {
+
     @Autowired
     private InventoryDto inventoryDto;
     @Autowired
     private ProductDto productDto;
 
     @Test
-    public void testAddInventory() throws ApiException{
+    public void testAddInventory() throws ApiException {
         String barcode = "abcdef12";
         int quantity = 10;
         String brand = "adidas";
@@ -42,13 +35,12 @@ public class InventoryTest extends AbstractUnitTest{
     }
 
     @Test
-    public void testAddInventoryBarcodeDoesntExist() throws ApiException{
+    public void testAddInventoryBarcodeDoesntExist() throws ApiException {
         String barcode = "abcdef12";
         int quantity = 10;
-        try{
+        try {
             TestUtil.createInventorySingle(barcode, quantity);
-        }
-        catch(ApiException e){
+        } catch (ApiException e) {
             return;
         }
         fail();
@@ -62,11 +54,10 @@ public class InventoryTest extends AbstractUnitTest{
         String category = "tshirts";
         String name = "polo";
         int mrp = 100;
-        
-        try{
+
+        try {
             TestUtil.createInventory(barcode, brand, category, name, mrp, quantity);
-        }
-        catch(ApiException e){
+        } catch (ApiException e) {
             return;
         }
         fail();
@@ -82,10 +73,9 @@ public class InventoryTest extends AbstractUnitTest{
         int mrp = 100;
         InventoryData p = TestUtil.createInventory(barcode, brand, category, name, mrp, quantity);
         boolean error = false;
-        try{
+        try {
             TestUtil.createInventorySingle(barcode, quantity);
-        }
-        catch(ApiException e){
+        } catch (ApiException e) {
             return;
         }
         fail();
@@ -103,7 +93,7 @@ public class InventoryTest extends AbstractUnitTest{
         ProductData productData = productDto.get(inventoryData.getId());
         assertEquals(quantity, inventoryData.getQuantity());
         assertEquals(productData.getBarcode(), barcode);
-        
+
         int newQuantity = 50;
         InventoryForm inventoryForm = TestUtil.getInventoryFormDto(barcode, newQuantity);
         inventoryDto.update(inventoryForm);
@@ -115,11 +105,10 @@ public class InventoryTest extends AbstractUnitTest{
     public void testUpdateInventoryDoesntExist() throws ApiException {
         String barcode = "abcdef12";
         int quantity = 10;
-        try{
+        try {
             InventoryForm inventoryForm = TestUtil.getInventoryFormDto(barcode, quantity);
             inventoryDto.update(inventoryForm);
-        }
-        catch(ApiException e){
+        } catch (ApiException e) {
             return;
         }
         fail();
@@ -138,5 +127,4 @@ public class InventoryTest extends AbstractUnitTest{
         assertEquals(quantity, inventoryData.getQuantity());
         assertEquals(barcode, inventoryData.getBarcode());
     }
-
 }
