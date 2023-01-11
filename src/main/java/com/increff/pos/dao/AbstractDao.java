1ba60c733ddb5a3a8a937dee888fc20ac7b0367c
abhaychandna <abhaychandna5@gmail.com>
2023-01-11 16:00:12 +0530

    BUGFIX: imports added

--- src/main/java/com/increff/pos/dao/AbstractDao.java
+++ src/main/java/com/increff/pos/dao/AbstractDao.java
@@ -1,22 +1,27 @@
 package com.increff.pos.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 public abstract class AbstractDao {
-	
+
 	@PersistenceContext
 	protected EntityManager em;
 
 	protected <T> T getSingle(TypedQuery<T> query) {
 		return query.getResultList().stream().findFirst().orElse(null);
 	}
-	
+
 	protected <T> TypedQuery<T> getQuery(String jpql, Class<T> clazz) {
 		return em.createQuery(jpql, clazz);
 	}
-	
+
 	protected EntityManager em() {
 		return em;
 	}
