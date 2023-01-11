e20e3b71218a33c09f22812c6462de95c633a9f1
abhaychandna <abhaychandna5@gmail.com>
2023-01-11 15:16:44 +0530

    InventoryPojo - column nullable = false added

--- src/main/java/com/increff/pos/pojo/InventoryPojo.java
+++ src/main/java/com/increff/pos/pojo/InventoryPojo.java
@@ -1,5 +1,6 @@
 package com.increff.pos.pojo;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -12,7 +13,9 @@ import lombok.Setter;
 public class InventoryPojo {
 
 	@Id
+	@Column(name="id", nullable=false)
 	private int id;
+	@Column(name="quantity", nullable=false)
 	private int quantity;
 
 }
