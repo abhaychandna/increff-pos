cd48b86804ffe527265656d775dd161a52eafcb1
abhaychandna <abhaychandna5@gmail.com>
2023-01-11 12:12:37 +0530

    UserSvc - @Transactional moved to class declaration

--- src/main/java/com/increff/pos/service/UserService.java
+++ src/main/java/com/increff/pos/service/UserService.java
@@ -11,12 +11,12 @@ import com.increff.pos.dao.UserDao;
 import com.increff.pos.pojo.UserPojo;
 
 @Service
+@Transactional(rollbackOn = ApiException.class)
 public class UserService {
 
 	@Autowired
 	private UserDao dao;
 
-	@Transactional
 	public void add(UserPojo p) throws ApiException {
 		normalize(p);
 		UserPojo existing = dao.select(p.getEmail());
@@ -26,17 +26,14 @@ public class UserService {
 		dao.insert(p);
 	}
 
-	@Transactional(rollbackOn = ApiException.class)
 	public UserPojo get(String email) throws ApiException {
 		return dao.select(email);
 	}
 
-	@Transactional
 	public List<UserPojo> getAll() {
 		return dao.selectAll();
 	}
 
-	@Transactional
 	public void delete(int id) {
 		dao.delete(id);
 	}
