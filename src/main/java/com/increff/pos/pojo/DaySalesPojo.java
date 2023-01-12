e5f9bbf1f701ee9d99272439a1d580e2d28d882a
abhaychandna <abhaychandna5@gmail.com>
2023-01-12 17:41:46 +0530

    DaySalesPojo added

---
+++ src/main/java/com/increff/pos/pojo/DaySalesPojo.java
@@ -0,0 +1,30 @@
+package com.increff.pos.pojo;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Table(indexes = {
+    @Index(name="dateIndex", columnList = "date"),
+})
+@Entity
+public class DaySalesPojo extends AbstractPojo{
+
+    @Id
+    @Column(name="date", nullable=false)
+    private Date date;
+    @Column(name="invoicedOrdersCount", nullable=false)
+    private Integer invoicedOrdersCount;
+    @Column(name="invoicedItemsCount", nullable=false)
+    private Integer invoicedItemsCount;
+    @Column(name="totalRevenue", nullable=false)
+    private Double totalRevenue;
+}
