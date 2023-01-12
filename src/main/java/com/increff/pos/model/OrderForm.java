2b815f9b60d2993999adadde0dc8d483b22de6e6
abhaychandna <abhaychandna5@gmail.com>
2023-01-12 10:27:34 +0530

    OrderItem/Order Form/Data added

---
+++ src/main/java/com/increff/pos/model/OrderForm.java
@@ -0,0 +1,14 @@
+package com.increff.pos.model;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderForm {
+
+    private Date time;
+
+}
---
+++ src/main/java/com/increff/pos/model/OrderItemData.java
@@ -0,0 +1,11 @@
+package com.increff.pos.model;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderItemData extends OrderItemForm {
+
+    private Integer id;
+    private Integer orderId;
+}
