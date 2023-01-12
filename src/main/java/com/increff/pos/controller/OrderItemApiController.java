a42782e32af09c2e9059a2e75c761b48578e0c75
abhaychandna <abhaychandna5@gmail.com>
2023-01-12 14:15:20 +0530

    OrderItemController added

---
+++ src/main/java/com/increff/pos/controller/OrderItemApiController.java
@@ -0,0 +1,52 @@
+package com.increff.pos.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.increff.pos.dto.OrderItemDto;
+import com.increff.pos.model.OrderItemData;
+import com.increff.pos.model.OrderItemForm;
+import com.increff.pos.model.OrderItemPutForm;
+import com.increff.pos.service.ApiException;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api
+@RestController
+@RequestMapping(path = "/api")
+public class OrderItemApiController {
+
+	@Autowired
+	private OrderItemDto dto;
+
+	@ApiOperation(value = "Adds a OrderItem")
+	@RequestMapping(path = "/OrderItems", method = RequestMethod.POST)
+	public void add(@RequestBody List<OrderItemForm> forms) throws ApiException {
+		dto.add(forms);
+	}
+
+	@ApiOperation(value = "Gets a OrderItem by ID")
+	@RequestMapping(path = "/OrderItems/{id}", method = RequestMethod.GET)
+	public OrderItemData get(@PathVariable Integer id) throws ApiException {
+		return dto.get(id);
+	}
+
+	@ApiOperation(value = "Gets all OrderItems")
+	@RequestMapping(path = "/OrderItems", method = RequestMethod.GET)
+	public List<OrderItemData> getAll(@RequestParam Integer pageNo, @RequestParam Integer pageSize) throws ApiException {
+		return dto.getAll(pageNo, pageSize);
+	}
+
+	@RequestMapping(path = "/OrderItems/{id}", method = RequestMethod.PUT)
+	public void update(@PathVariable Integer id, @RequestBody OrderItemPutForm f) throws ApiException {
+		dto.update(id, f);
+	}
+}
