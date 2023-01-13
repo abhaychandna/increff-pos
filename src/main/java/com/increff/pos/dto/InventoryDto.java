a6984a89b00a7ba6a23b4e034a16da7fbb856d79
abhaychandna <abhaychandna5@gmail.com>
2023-01-13 14:26:25 +0530

    InventoryDto convert db calls moved to dto layer

--- src/main/java/com/increff/pos/dto/InventoryDto.java
+++ src/main/java/com/increff/pos/dto/InventoryDto.java
@@ -11,6 +11,7 @@ import com.increff.pos.model.InventoryForm;
 import com.increff.pos.pojo.InventoryPojo;
 import com.increff.pos.service.ApiException;
 import com.increff.pos.service.InventoryService;
+import com.increff.pos.service.ProductService;
 import com.increff.pos.util.ConvertUtil;
 import com.increff.pos.util.NormalizeUtil;
 import com.increff.pos.util.ValidateUtil;
@@ -19,39 +20,53 @@ import com.increff.pos.util.ValidateUtil;
 public class InventoryDto {
 
     @Autowired
-    InventoryService svc;
+    private InventoryService svc;
+    @Autowired
+    private ProductService productService;
 
     public InventoryData add(InventoryForm inventoryForm) throws ApiException {
-        InventoryPojo inventory = ConvertUtil.convert(inventoryForm);
+        InventoryPojo inventory = convert(inventoryForm);
         NormalizeUtil.normalize(inventory);
         ValidateUtil.validateInventory(inventory);
         try {
             svc.get(inventory.getId());
         } catch (ApiException e) {
             inventory = svc.add(inventory);
-            return ConvertUtil.convert(inventory);
+            return convert(inventory);
         }
         throw new ApiException("Inventory with given Barcode already exists");
     }
 
     public InventoryData get(Integer id) throws ApiException {
         InventoryPojo inventory = svc.get(id);
-        return ConvertUtil.convert(inventory);
+        return convert(inventory);
     }
 
     public List<InventoryData> getAll(Integer pageNo, Integer pageSize) throws ApiException {
         List<InventoryPojo> brands = svc.getAll(pageNo, pageSize);
         List<InventoryData> respList = new ArrayList<InventoryData>();
         for (InventoryPojo p : brands) {
-            respList.add(ConvertUtil.convert(p));
+            respList.add(convert(p));
         }
         return respList;
     }
 
     public void update(InventoryForm f) throws ApiException {
-        InventoryPojo inventory = ConvertUtil.convert(f);
+        InventoryPojo inventory = convert(f);
         NormalizeUtil.normalize(inventory);
         ValidateUtil.validateInventory(inventory);
         svc.update(inventory.getId(), inventory);
+    }
+
+    private InventoryPojo convert(InventoryForm inventoryForm) throws ApiException {
+        InventoryPojo inventory = ConvertUtil.convert(inventoryForm, InventoryPojo.class);
+        inventory.setId(productService.getByBarcode(inventoryForm.getBarcode()).getId());
+        return inventory;
+    }
+
+    private InventoryData convert(InventoryPojo inventoryPojo) throws ApiException {
+        InventoryData inventory = ConvertUtil.convert(inventoryPojo, InventoryData.class);
+        inventory.setBarcode(productService.get(inventoryPojo.getId()).getBarcode());
+        return inventory;
     }
 }
