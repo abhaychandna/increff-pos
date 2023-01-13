6201964d17c8ddb13032217b847c215d54d57246
abhaychandna <abhaychandna5@gmail.com>
2023-01-13 15:14:09 +0530

    ProductDto convert db calls moved to dto layer

--- src/main/java/com/increff/pos/dto/ProductDto.java
+++ src/main/java/com/increff/pos/dto/ProductDto.java
@@ -8,8 +8,10 @@ import org.springframework.stereotype.Component;
 
 import com.increff.pos.model.ProductData;
 import com.increff.pos.model.ProductForm;
+import com.increff.pos.pojo.BrandPojo;
 import com.increff.pos.pojo.ProductPojo;
 import com.increff.pos.service.ApiException;
+import com.increff.pos.service.BrandService;
 import com.increff.pos.service.ProductService;
 import com.increff.pos.util.ConvertUtil;
 import com.increff.pos.util.NormalizeUtil;
@@ -20,34 +22,49 @@ public class ProductDto {
 
     @Autowired
     private ProductService svc;
-
+    @Autowired
+    private BrandService brandService;
+    
     public ProductData add(ProductForm form) throws ApiException {
-        ProductPojo product = ConvertUtil.convert(form);
+        ProductPojo product = convert(form);
         NormalizeUtil.normalize(product);
         ValidateUtil.validateProduct(product);
         ProductPojo productPojo = svc.add(product);
-        return ConvertUtil.convert(productPojo);
+        return convert(productPojo);
     }
 
     public ProductData get(Integer id) throws ApiException {
         ProductPojo product = svc.get(id);
-        return ConvertUtil.convert(product);
+        return convert(product);
     }
 
     public List<ProductData> getAll(Integer pageNo, Integer pageSize) throws ApiException {
-        List<ProductPojo> Products = svc.getAll(pageNo, pageSize);
-        List<ProductData> respList = new ArrayList<ProductData>();
-        for (ProductPojo p : Products) {
-            respList.add(ConvertUtil.convert(p));
+        List<ProductPojo> products = svc.getAll(pageNo, pageSize);
+        List<ProductData> productList = new ArrayList<ProductData>();
+        for (ProductPojo p : products) {
+            productList.add(convert(p));
         }
-        return respList;
+        return productList;
     }
 
     public void update(Integer id, ProductForm form) throws ApiException {
-        ProductPojo product = ConvertUtil.convert(form);
+        ProductPojo product = convert(form);
         NormalizeUtil.normalize(product);
         ValidateUtil.validateProduct(product);
         svc.update(id, product);
     }
 
+    private ProductPojo convert(ProductForm productForm) throws ApiException{
+		ProductPojo product = ConvertUtil.convert(productForm, ProductPojo.class);
+		product.setBrandCategory(brandService.getCheckBrandCategory(productForm.getBrand(), productForm.getCategory()).getId());
+		return product;
+	}
+
+    private ProductData convert(ProductPojo product) throws ApiException{
+        ProductData productData = ConvertUtil.convert(product, ProductData.class);
+        BrandPojo brand = brandService.get(product.getBrandCategory());
+        productData.setBrand(brand.getBrand());
+        productData.setCategory(brand.getCategory());
+        return productData;
+    }
 }
