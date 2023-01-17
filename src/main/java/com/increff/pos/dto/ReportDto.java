884e6307efa2ce0d00a5b11922673c8264d9b238
abhaychandna <abhaychandna5@gmail.com>
2023-01-17 11:30:23 +0530

    salesReport without filters added

--- src/main/java/com/increff/pos/dto/ReportDto.java
+++ src/main/java/com/increff/pos/dto/ReportDto.java
@@ -10,18 +10,18 @@ import org.springframework.stereotype.Component;
 import com.increff.pos.model.SalesReportData;
 import com.increff.pos.pojo.BrandPojo;
 import com.increff.pos.pojo.InventoryPojo;
+import com.increff.pos.pojo.OrderItemPojo;
+import com.increff.pos.pojo.OrderPojo;
 import com.increff.pos.service.ApiException;
 import com.increff.pos.service.BrandService;
 import com.increff.pos.service.InventoryService;
+import com.increff.pos.service.OrderItemService;
+import com.increff.pos.service.OrderService;
 import com.increff.pos.service.ProductService;
-import com.increff.pos.service.ReportService;
 
 
 @Component
 public class ReportDto {
-    
-    @Autowired
-    ReportService svc;
 
     @Autowired
     private BrandService brandService;
@@ -29,6 +29,10 @@ public class ReportDto {
     private ProductService productService;
     @Autowired
     private InventoryService inventoryService;
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private OrderItemService orderItemService;
 
 
     public List<SalesReportData> inventoryReport() throws ApiException {
@@ -60,7 +64,51 @@ public class ReportDto {
 		return inventoryReportDatas;
 	}
 
+    public List<SalesReportData> salesReport(String startDate, String endDate, String brand, String category) throws ApiException {
+        // TODO : Convert Date String to ZonedDateTime
+        List<SalesReportData> salesReportDatas = new ArrayList<SalesReportData>();
+        // TODO : Add date filters
+        List<OrderPojo> orders = orderService.getAll(0, 1000);
+        
+        HashMap<Integer, List<String>> brandCategoryMap = new HashMap<>();
+        HashMap<Integer, Integer> quantityMap = new HashMap<>();
+        HashMap<Integer, Double> revenueMap = new HashMap<>();
 
+        for (OrderPojo order:  orders){
+            Integer orderId = order.getId();
+            List<OrderItemPojo> orderItems = orderItemService.getByOrderId(orderId);
+            for (OrderItemPojo orderItem: orderItems){
+                Integer productId = orderItem.getProductId();
+                if(!brandCategoryMap.containsKey(productId)){
+                    Integer brandCategoryId = productService.get(productId).getBrandCategory();
+                    BrandPojo brandPojo = brandService.get(brandCategoryId);
+                    brandCategoryMap.put(productId, List.of(brandPojo.getBrand(), brandPojo.getCategory()));
+                }
+                // TODO : Add brand and category filters
+                Integer quantity = orderItem.getQuantity();
+                Double revenue = orderItem.getSellingPrice();
+                if(quantityMap.containsKey(productId)){
+                    quantity += 1;
+                    revenue += revenueMap.get(productId);
+                }
+                quantityMap.put(productId, quantity);
+                revenueMap.put(productId, revenue);
+            }
+        }
+
+        for(Integer productId : quantityMap.keySet()){
+            List<String> brandCategory = brandCategoryMap.get(productId);
+            SalesReportData salesReportData = new SalesReportData();
+            salesReportData.setBrand(brandCategory.get(0));
+            salesReportData.setCategory(brandCategory.get(1));
+            salesReportData.setQuantity(quantityMap.get(productId));
+            salesReportData.setRevenue(revenueMap.get(productId));
+            // print all 
+            System.out.println(salesReportData.getBrand() + " " + salesReportData.getCategory() + " " + salesReportData.getQuantity() + " " + salesReportData.getRevenue());
+            salesReportDatas.add(salesReportData);
+        }
+        return salesReportDatas;
+    }
 
 
 }
---
+++ src/main/java/com/increff/pos/model/SalesReportData.java
@@ -0,0 +1,12 @@
+package com.increff.pos.model;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SalesReportData {
+	private String brand;
+	private String category;
+	private Integer quantity;
+	private Double revenue;
+}
