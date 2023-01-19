38a9eae4a83ecba0433b708aae1dab34641d1653
abhaychandna <abhaychandna5@gmail.com>
2023-01-19 15:20:08 +0530

    SalesReportData constructor added

--- src/main/java/com/increff/pos/model/SalesReportData.java
+++ src/main/java/com/increff/pos/model/SalesReportData.java
@@ -9,4 +9,10 @@ public class SalesReportData {
 	private String category;
 	private Integer quantity;
 	private Double revenue;
+	public SalesReportData(String brand, String category, Integer quantity, Double revenue) {
+		this.brand = brand;
+		this.category = category;
+		this.quantity = quantity;
+		this.revenue = revenue;
+	}
 }
