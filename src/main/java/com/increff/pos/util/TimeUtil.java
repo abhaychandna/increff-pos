78b73c7c330b10024b86384d2032a7908ba1faa5
abhaychandna <abhaychandna5@gmail.com>
2023-01-20 12:32:03 +0530

    TimeUtil added

---
+++ src/main/java/com/increff/pos/util/TimeUtil.java
@@ -0,0 +1,17 @@
+package com.increff.pos.util;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TimeUtil {
+    
+    public static String getFormattedTime(ZonedDateTime time, String format) {
+        return time.format(DateTimeFormatter.ofPattern(format));
+    }
+
+    public static ZonedDateTime getZonedDateTime(String dateTime, DateTimeFormatter format) {
+        ZonedDateTime zdt = ZonedDateTime.parse(dateTime, format);
+        System.out.println("after conversion " + zdt);
+        return zdt;
+    }
+}
