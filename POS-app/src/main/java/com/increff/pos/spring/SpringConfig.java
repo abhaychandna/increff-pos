0523e2cdd5dc2495c9683abaa71ad8ea4ee4bbd1
abhaychandna <abhaychandna5@gmail.com>
2023-01-23 17:51:54 +0530

    Scheduling config added

--- POS-app/src/main/java/com/increff/pos/spring/SpringConfig.java
+++ POS-app/src/main/java/com/increff/pos/spring/SpringConfig.java
@@ -9,8 +9,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
+@EnableScheduling
+@EnableAsync
 @ComponentScan("com.increff.pos")
 @PropertySources({ //
 		@PropertySource(value = "file:./pos.properties", ignoreResourceNotFound = true) //
