b9d076d41849ca7d96f94a6d77f12834afdb68f7
abhaychandna <abhaychandna5@gmail.com>
2023-01-27 11:14:42 +0530

    ProductTest -     @BeforeClass initializations added

--- POS-app/src/test/java/com/increff/pos/dto/ProductTest.java
+++ POS-app/src/test/java/com/increff/pos/dto/ProductTest.java
@@ -3,12 +3,15 @@ package com.increff.pos.dto;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.util.List;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.increff.pos.dao.ProductDao;
 import com.increff.pos.model.BrandData;
-import com.increff.pos.model.BrandForm;
 import com.increff.pos.model.PaginatedData;
 import com.increff.pos.model.ProductData;
 import com.increff.pos.model.ProductForm;
@@ -23,29 +26,29 @@ public class ProductTest extends AbstractUnitTest{
     private ProductDto productDto;
     @Autowired
     private ProductDao productDao;
-    @Autowired 
-    private BrandDto brandDto;
     
     private Double tolerance = 1e-6;
     
-
+    private static String brand;
+    private static String category;
+    private static String barcode;
+    private static String name;
+    private static Double mrp;
+    @BeforeClass
+    public static void init() throws ApiException{
+        brand = "adidas";
+        category = "tshirts";
+        barcode = "abcdef12";
+        name = "polo";
+        mrp = 100.5;
+    }
 
     @Test
     public void testAddProduct() throws ApiException{
-        String brand = "adidas";
-        String category = "tshirts";
-        String barcode = "abcdef12";
-        String name = "polo";
-        Double mrp = 100.5;
-        
-        //ProductPojo p = createProductWithBrand(barcode, brand, category, name, mrp)
-
-        BrandForm brandForm = TestUtil.getBrandFormDto(brand,category);
-        BrandData brandData = brandDto.add(brandForm);
-
+        BrandData brandData = TestUtil.createBrand(brand, category);
         ProductForm productForm = TestUtil.getProductFormDto(barcode,brand,category,name,mrp);
         ProductData productData = productDto.add(productForm);
-
+        
         ProductPojo product = productDao.select(ProductPojo.class, productData.getId());
         assertEquals(brandData.getId(), product.getBrandCategory());
         assertEquals(barcode, product.getBarcode());
@@ -55,12 +58,6 @@ public class ProductTest extends AbstractUnitTest{
 
     @Test
     public void testAddBrandCategoryDoesNotExist() throws ApiException{
-        String brand = "adidas";
-        String category = "tshirts";
-        String barcode = "abcdef12";
-        String name = "polo";
-        Double mrp = 100.5;
-
         ProductForm productForm = TestUtil.getProductFormDto(barcode,brand,category,name,mrp);
         try{
             productDto.add(productForm);
@@ -73,12 +70,6 @@ public class ProductTest extends AbstractUnitTest{
 
     @Test
     public void testDuplicateBarcode() throws ApiException{
-        String brand = "adidas";
-        String category = "tshirts";
-        String barcode = "abcdef12";
-        String name = "polo";
-        Double mrp = 100.5;
-
         TestUtil.createProductWithBrand(barcode, brand, category, name, mrp);
         ProductForm productForm = TestUtil.getProductFormDto(barcode,brand,category,name,mrp);
         try{
@@ -92,14 +83,8 @@ public class ProductTest extends AbstractUnitTest{
     
     @Test
     public void testGet() throws ApiException {
-        String brand = "adidas";
-        String category = "tshirts";
-        String barcode = "abcdef12";
-        String name = "polo";
-        Double mrp = 100.5;
-
-        ProductData productData = TestUtil.createProductWithBrand(barcode, brand, category, name, mrp);
-        ProductPojo product = productDao.select(ProductPojo.class, productData.getId());
+        ProductPojo product = TestUtil.createProductWithBrand(barcode, brand, category, name, mrp);
+        ProductData productData = productDto.get(product.getId());
         
         assertEquals(product.getId(), productData.getId());
         assertEquals(brand, productData.getBrand());
@@ -111,12 +96,6 @@ public class ProductTest extends AbstractUnitTest{
 
     @Test
     public void testGetAll() throws ApiException {
-        String brand = "adidas";
-        String category = "tshirts";
-        String barcode = "abcdef12";
-        String name = "polo";
-        Double mrp = 100.5;
-
         TestUtil.createProductWithBrand(barcode, brand, category, name, mrp);
         
         TestUtil.createProduct("abcdef13", brand, category, name, mrp);
@@ -126,33 +105,17 @@ public class ProductTest extends AbstractUnitTest{
 
     @Test
     public void testUpdate() throws ApiException {
-        String brand = "adidas";
-        String category = "tshirts";
-        String barcode = "abcdef12";
-        String name = "polo";
-        Double mrp = 100.5;
-
-        ProductData productData = TestUtil.createProductWithBrand(barcode, brand, category, name, mrp);
-        ProductPojo product = productDao.select(ProductPojo.class, productData.getId());
-        assertEquals(product.getId(), productData.getId());
-        assertEquals(brand, productData.getBrand());
-        assertEquals(category, productData.getCategory());
-        assertEquals(product.getBarcode(), productData.getBarcode());
-        assertEquals(product.getName(), productData.getName());
-        assertEquals(product.getMrp(), productData.getMrp(), tolerance);
+        ProductPojo product = TestUtil.createProductWithBrand(barcode, brand, category, name, mrp);
+        ProductData productData = productDto.get(product.getId());
         
-        String newBrand = "nike";
-        String newCategory = "shoes";
-        String newBarcode = "abcdef13";
-        String newName = "polo";
+        String newBrand = "nike", newCategory = "shoes", newBarcode = "abcdef13", newName = "polo";
         Double newMrp = 200.5;
         
         TestUtil.createBrand(newBrand, newCategory);
-        ProductForm f = TestUtil.getProductFormDto(newBarcode,newBrand,newCategory,newName,newMrp);
-        productDto.update(productData.getId(), f);
+        ProductForm form = TestUtil.getProductFormDto(newBarcode,newBrand,newCategory,newName,newMrp);
+        productDto.update(productData.getId(), form);
         
         productData = productDto.get(product.getId());
-        
         assertEquals(product.getId(), productData.getId());
         assertEquals(newBrand, productData.getBrand());
         assertEquals(newCategory, productData.getCategory());
