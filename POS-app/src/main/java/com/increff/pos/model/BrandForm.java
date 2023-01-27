41bdca5858e1eccee6360ae1fb82f17be4bdb33c
abhaychandna <abhaychandna5@gmail.com>
2023-01-27 10:25:33 +0530

    BrandForm - blank error msg cahnged

--- POS-app/src/main/java/com/increff/pos/model/BrandForm.java
+++ POS-app/src/main/java/com/increff/pos/model/BrandForm.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 @Setter
 public class BrandForm {
 
-	@NotBlank(message = "Name may not be empty")
+	@NotBlank(message = "Brand may not be empty")
 	private String brand;
 	@NotBlank(message = "Category may not be empty")
 	private String category;
