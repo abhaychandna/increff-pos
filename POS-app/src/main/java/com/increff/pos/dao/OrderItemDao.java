4a0e774254d438a46a3d3176318c2e3e5ef320f2
abhaychandna <abhaychandna5@gmail.com>
2023-01-30 09:58:28 +0530

    OrderItemDao.selectByOrderId refactored to selectMultiple

--- POS-app/src/main/java/com/increff/pos/dao/OrderItemDao.java
+++ POS-app/src/main/java/com/increff/pos/dao/OrderItemDao.java
@@ -1,14 +1,6 @@
 package com.increff.pos.dao;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 
@@ -18,11 +10,6 @@ import com.increff.pos.service.ApiException;
 @Repository
 public class OrderItemDao extends AbstractDao {
 	public List<OrderItemPojo> selectByOrderId(Integer id) throws ApiException {
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<OrderItemPojo> cq = cb.createQuery(OrderItemPojo.class);
-        Root<OrderItemPojo> root = cq.from(OrderItemPojo.class);
-       	cq.where(cb.equal(root.get("orderId"), id));
-        TypedQuery<OrderItemPojo> query = em.createQuery(cq);
-		return query.getResultList();
+        return selectMultiple(OrderItemPojo.class, "orderId", id);
 	}
 }
