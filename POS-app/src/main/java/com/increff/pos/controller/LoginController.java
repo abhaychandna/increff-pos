fb27ed1973c17c89b402e5b15d78f52cf2e7ec4e
abhaychandna <abhaychandna5@gmail.com>
2023-02-02 17:50:45 +0530

    Login controller - /session moved to global request mapping

--- POS-app/src/main/java/com/increff/pos/controller/LoginController.java
+++ POS-app/src/main/java/com/increff/pos/controller/LoginController.java
@@ -5,9 +5,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.increff.pos.dto.UserDto;
@@ -15,9 +15,12 @@ import com.increff.pos.model.LoginForm;
 import com.increff.pos.model.SignupForm;
 import com.increff.pos.service.ApiException;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
-@Controller
+@Api
+@RestController
+@RequestMapping(path = "/session")
 public class LoginController {
 	
 	@Autowired
@@ -25,19 +28,19 @@ public class LoginController {
 
 	
 	@ApiOperation(value = "Logs in a user")
-	@RequestMapping(path = "/session/login", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	@RequestMapping(path = "/login", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
 	public ModelAndView login(HttpServletRequest req, LoginForm form) throws ApiException {
 		return dto.login(req, form);
 	}
 
-	@RequestMapping(path = "/session/logout", method = RequestMethod.GET)
+	@RequestMapping(path = "/logout", method = RequestMethod.GET)
 	public ModelAndView logout(HttpServletRequest request, HttpServletResponse response) {
 		request.getSession().invalidate();
 		return new ModelAndView("redirect:/site/login");
 	}
 
 	@ApiOperation(value = "Signs up a user")
-	@RequestMapping(path = "/session/signup", method = RequestMethod.POST)
+	@RequestMapping(path = "/signup", method = RequestMethod.POST)
 	public ModelAndView signup(HttpServletRequest req, SignupForm form) throws ApiException {
 		return dto.signup(req, form);
 	}	
