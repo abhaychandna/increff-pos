d1459bb0761bf553de9dfee768b41185ed391ebd
abhaychandna <abhaychandna5@gmail.com>
2023-02-03 11:15:36 +0530

    UserService Tests added

---
+++ POS-app/src/test/java/com/increff/pos/service/UserServiceTest.java
@@ -0,0 +1,60 @@
+package com.increff.pos.service;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import com.increff.pos.dao.UserDao;
+import com.increff.pos.model.Role;
+import com.increff.pos.pojo.UserPojo;
+
+public class UserServiceTest extends AbstractUnitTest {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserDao userDao;
+
+    @Value("${supervisorEmail}")
+    private String supervisorEmail;
+
+    private static String email;
+    private static String password;
+    @BeforeClass
+    public static void init() throws ApiException {
+        email = "test@test.com";
+        password = "testPassword";
+    }
+
+    @Test
+    public void testAdd() throws ApiException {
+        UserPojo user = new UserPojo(email, password, Role.operator);
+        userService.add(user);
+        UserPojo userGet = userDao.select(UserPojo.class, user.getId());
+        assertEquals(user, userGet);
+    }
+
+    @Test
+    public void testGet() throws ApiException {
+        UserPojo user = new UserPojo(email, password, Role.operator);
+        userDao.insert(user);
+        UserPojo userGet = userService.get(email);
+        assertEquals(user, userGet);
+    }
+
+    @Test
+    public void testGetAll() throws ApiException {
+        UserPojo user = new UserPojo(email, password, Role.operator);
+        userDao.insert(user);
+        UserPojo user2 = new UserPojo("email2@gmail.com", "password", Role.supervisor);
+        userDao.insert(user2);
+        List<UserPojo> userGet = userService.getAll();
+        assertEquals(user, userGet.get(0));
+        assertEquals(user2, userGet.get(1));
+    }
+}
