c6e6c2d6f403d812e7b4cd3b90ac4c568c6b19d4
abhaychandna <abhaychandna5@gmail.com>
2023-02-03 18:39:28 +0530

    UserTests added

---
+++ POS-app/src/test/java/com/increff/pos/dto/UserTest.java
@@ -0,0 +1,70 @@
+package com.increff.pos.dto;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.increff.pos.dao.UserDao;
+import com.increff.pos.model.SignupForm;
+import com.increff.pos.pojo.UserPojo;
+import com.increff.pos.service.AbstractUnitTest;
+import com.increff.pos.service.ApiException;
+import com.increff.pos.service.TestUtil;
+import com.increff.pos.service.UserService;
+
+public class UserTest extends AbstractUnitTest {
+
+    @Autowired
+    private UserDto userDto;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserDao userDao;
+    @Autowired
+    private TestUtil testUtil;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Value("${supervisorEmail}")
+    private String supervisorEmail;
+
+    private String email;
+    private String password;
+    @Before
+    public void init() throws ApiException {
+        email = "test@test.com";
+        password = "test21i39812asd";
+    }
+
+    @Test
+    public void testSignup() throws ApiException {
+        SignupForm form = testUtil.getSignupFormDto(email, password);
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        userDto.signup(request, form);
+        UserPojo user = userDao.selectByColumn(UserPojo.class, "email", email);
+        checkEquals(user, form);
+    }
+
+    @Test
+    public void testSignupSupervisor() throws ApiException {
+        SignupForm form = testUtil.getSignupFormDto(supervisorEmail, password);
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        userDto.signup(request, form);
+        UserPojo user = userService.get(supervisorEmail);
+        checkEquals(user, form); 
+        assertEquals("supervisor", user.getRole().toString());
+    }    
+
+    private void checkEquals(UserPojo user, SignupForm form) {
+        assertEquals(user.getEmail(), form.getEmail());
+        assertTrue(passwordEncoder.matches(form.getPassword(), user.getPassword()));
+    }
+
+
+}
