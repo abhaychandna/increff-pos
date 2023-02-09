30b94f5e9a781e7475563834c5b7a5a45c19d570
abhaychandna <abhaychandna5@gmail.com>
2023-02-09 12:31:03 +0530

    PDFForm - xsltFilename changed from enum to string

--- PDF-app/src/main/java/com/increff/pdf/model/PDFForm.java
+++ PDF-app/src/main/java/com/increff/pdf/model/PDFForm.java
@@ -3,18 +3,16 @@ package com.increff.pdf.model;
 import java.util.HashMap;
 import java.util.List;
 
-import com.increff.pdf.model.data.XSLTFilname;
-
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class PDFForm<T> {
-    private XSLTFilname xsltFilename;
+    private String xsltFilename;
     private HashMap<String, String> headers;
     private List<T> reportData;
-    public PDFForm(XSLTFilname xsltFilename, HashMap<String, String> headers, List<T> reportData) {
+    public PDFForm(String xsltFilename, HashMap<String, String> headers, List<T> reportData) {
         this.xsltFilename = xsltFilename;
         this.headers = headers;
         this.reportData = reportData;
