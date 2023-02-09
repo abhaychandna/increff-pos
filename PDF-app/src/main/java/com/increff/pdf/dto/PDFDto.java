363cfb69fbfea3b76a0e3c47cb94605cece0c492
abhaychandna <abhaychandna5@gmail.com>
2023-02-09 13:46:10 +0530

    PDF app - form validations addded

--- PDF-app/src/main/java/com/increff/pdf/dto/PDFDto.java
+++ PDF-app/src/main/java/com/increff/pdf/dto/PDFDto.java
@@ -1,16 +1,18 @@
 package com.increff.pdf.dto;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.increff.pdf.model.PDFForm;
+import com.increff.pdf.model.data.XSLTFilename;
 import com.increff.pdf.service.ApiException;
 import com.increff.pdf.util.PDFUtil;
-import com.increff.pdf.util.PreProcessingUtil;
 import com.increff.pdf.util.XMLUtil;
 
 @Component
@@ -19,7 +21,7 @@ public class PDFDto {
     private String xmlDirectory;
     
     public <T> String generateReport(PDFForm<T> pdfForm) throws ApiException {
-        PreProcessingUtil.validate(pdfForm);
+        validate(pdfForm);
         List<T> reportForm = pdfForm.getReportData();
         String xsltFilename = pdfForm.getXsltFilename().toString();
         HashMap<String, String> headers = pdfForm.getHeaders();
@@ -38,5 +40,26 @@ public class PDFDto {
             e.printStackTrace();
             throw new ApiException("Error in generating report." + e.getMessage());
         }
+    }
+
+    private <T> void validate(PDFForm<T> pdfForm) throws ApiException {
+        List<String> errors = new ArrayList<String>();
+        if (pdfForm.getXsltFilename() == null) {
+            errors.add("XSLT filename is null");
+        }
+        else{
+            List<String> enumValues = List.of(XSLTFilename.class.getEnumConstants()).stream().map(String::valueOf).collect(Collectors.toList());
+            if (!enumValues.contains(pdfForm.getXsltFilename())) {
+                errors.add("XSLT filename is not valid");
+                errors.add("Valid values are: " + String.join(", ", enumValues));
+            }
+        }
+        if (pdfForm.getReportData() == null) {
+            errors.add("Report data is null");
+        }
+        if(errors.size() > 0){
+            throw new ApiException(String. join(".\n", errors));
+        }
+
     }
 }
