ecdc8687b4796151abea4705f8efcc1acc431547
abhaychandna <abhaychandna5@gmail.com>
2023-02-09 11:25:35 +0530

    DaySalesServiceTest - creating daySales directly instead of from orders

--- POS-app/src/test/java/com/increff/pos/service/DaySalesServiceTest.java
+++ POS-app/src/test/java/com/increff/pos/service/DaySalesServiceTest.java
@@ -4,8 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,12 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.increff.pos.config.AbstractUnitTest;
 import com.increff.pos.dao.DaySalesDao;
-import com.increff.pos.dao.OrderDao;
-import com.increff.pos.dao.ProductDao;
-import com.increff.pos.model.form.OrderItemForm;
 import com.increff.pos.pojo.DaySalesPojo;
-import com.increff.pos.pojo.OrderItemPojo;
-import com.increff.pos.pojo.OrderPojo;
 import com.increff.pos.util.TestUtil;
 
 public class DaySalesServiceTest extends AbstractUnitTest {
@@ -27,104 +20,50 @@ public class DaySalesServiceTest extends AbstractUnitTest {
     private DaySalesService daySalesService;
     @Autowired
     private DaySalesDao daySalesDao;
-    @Autowired
-    private OrderDao orderDao;
-    @Autowired
-    private ProductDao productDao;
     
     @Autowired
     private TestUtil testUtil;
 
-    private String brand;
-    private String category;
-    private String barcode;
-    private String name;
-    private Double mrp;
-    private Integer quantity1;
-    private Integer quantity2;
-    private String barcode2; 
-    private OrderItemForm orderItemForm;
-    private OrderItemForm orderItemForm2;
-    private List<OrderItemForm> orderItemFormList;
-    private List<OrderItemPojo> orderItemPojoList;
-    private OrderPojo order;
-    private Double sellingPrice1;
-    private Double sellingPrice2;
+    private ZonedDateTime today;
+    private Integer invoicedOrdersCount;
+    private Integer invoicedItemsCount;
+    private Double totalRevenue;
+    
     @Before
     public void init() throws ApiException {
-        brand = "adidas";
-        category = "tshirts";
-        barcode = "abcdef12";
-        name = "polo";
-        mrp = 100.0;
-        quantity1 = 10;
-        quantity2 = 20;
-        sellingPrice1 = 1.11;
-        sellingPrice2 = 2.22;
-
-        testUtil.createInventoryCascade(barcode, brand, category, name, mrp, quantity1);
-        barcode2 = "abcdef13";
-        testUtil.createProduct(barcode2, brand, category, name, mrp);
-        testUtil.createInventory(barcode2, quantity1);
-
-        orderItemForm = new OrderItemForm(barcode, quantity1, sellingPrice1);
-        orderItemForm2 = new OrderItemForm(barcode2, quantity2, sellingPrice2);
-        orderItemFormList = List.of(orderItemForm, orderItemForm2);
-        orderItemPojoList = new ArrayList<OrderItemPojo>();
-        
-        order = new OrderPojo();
-        order.setTime(ZonedDateTime.now());
-        orderDao.insert(order);
-
-        orderItemFormList.forEach(orderItemForm -> {
-            Integer productId = productDao.selectByColumn("barcode", orderItemForm.getBarcode()).getId();
-            OrderItemPojo orderItem = new OrderItemPojo(productId, orderItemForm.getQuantity(), orderItemForm.getSellingPrice());
-            orderItem.setOrderId(order.getId());
-            orderItemPojoList.add(orderItem);
-        });
+        today = ZonedDateTime.now();
+        invoicedOrdersCount = 2;
+        invoicedItemsCount = 5;
+        totalRevenue = 1234.56;
     }
 
     @Test
-    public void testAdd() throws ApiException {
-        DaySalesPojo daySales = daySalesService.add(orderItemPojoList);
+    public void testUpdate() throws ApiException {
+        DaySalesPojo daySales = daySalesService.update(new DaySalesPojo(today, invoicedOrdersCount, invoicedItemsCount, totalRevenue));
         daySales = daySalesDao.select(daySales.getDate());
-        Double expectedRevenue = sellingPrice1 * quantity1 + sellingPrice2 * quantity2;
-        assertEquals(orderItemPojoList.size(), daySales.getInvoicedItemsCount());
-        assertEquals(1, daySales.getInvoicedOrdersCount());
-        assertEquals(expectedRevenue, daySales.getTotalRevenue());
+        assertEquals(today, daySales.getDate());
+        assertEquals(invoicedOrdersCount, daySales.getInvoicedOrdersCount());
+        assertEquals(invoicedItemsCount, daySales.getInvoicedItemsCount());
+        assertEquals(totalRevenue, daySales.getTotalRevenue());
     }
 
     @Test
-    public void testAddExisting_thenUpdate() throws ApiException {
-        DaySalesPojo daySales = daySalesService.add(orderItemPojoList);
+    public void testUpdateNonExistingDate_thenAddNewDate() throws ApiException {
+        DaySalesPojo daySales = daySalesService.update(new DaySalesPojo(today, invoicedOrdersCount, invoicedItemsCount, totalRevenue));
         Integer expectedItemsCount = 3, expectedOrdersCount = 2;
         Double expectedRevenue = 1000.25;
         DaySalesPojo newDaySales = new DaySalesPojo(daySales.getDate(), expectedOrdersCount, expectedItemsCount, expectedRevenue);
-        daySalesService.add(newDaySales);
+        daySalesService.update(newDaySales);
         daySales = daySalesDao.select(newDaySales.getDate());
-        System.out.println(daySales.getDate().toString() + daySales.getInvoicedItemsCount() + daySales.getInvoicedOrdersCount() + daySales.getTotalRevenue());
         assertEquals(newDaySales.getDate(), daySales.getDate());
         assertEquals(expectedItemsCount, daySales.getInvoicedItemsCount());
         assertEquals(expectedOrdersCount, daySales.getInvoicedOrdersCount());
         assertEquals(expectedRevenue, daySales.getTotalRevenue());
     }
-    
-    @Test
-    public void testUpdate() throws ApiException {
-        DaySalesPojo daySales = daySalesService.add(orderItemPojoList);
-        daySales = daySalesDao.select(daySales.getDate());
-        
-        daySales = daySalesService.update(orderItemPojoList);
-        daySales = daySalesDao.select(daySales.getDate());
-        Double expectedRevenue = (sellingPrice1 * quantity1 + sellingPrice2 * quantity2) * 2;
-        assertEquals(orderItemPojoList.size() * 2, daySales.getInvoicedItemsCount());
-        assertEquals(2, daySales.getInvoicedOrdersCount());
-        assertEquals(expectedRevenue, daySales.getTotalRevenue());
-    }
 
     @Test
     public void testGetCheck() throws ApiException {
-        DaySalesPojo daySales = daySalesService.add(orderItemPojoList);
+        DaySalesPojo daySales = daySalesService.update(new DaySalesPojo(today, invoicedOrdersCount, invoicedItemsCount, totalRevenue));
         daySales = daySalesDao.select(daySales.getDate());
         DaySalesPojo daySalesGet = daySalesService.getCheck(daySales.getDate());
         assertEquals(daySales, daySalesGet);
