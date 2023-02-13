e4fb0a9b835eb321f0bdead90d5a28ef98045a71
abhaychandna <abhaychandna5@gmail.com>
2023-02-13 12:50:14 +0530

    serialVersionUID removed

--- POS-app/src/main/java/com/increff/pos/util/ApiException.java
+++ POS-app/src/main/java/com/increff/pos/util/ApiException.java
@@ -2,8 +2,6 @@ package com.increff.pos.util;
 
 public class ApiException extends Exception {
 
-	private static final Long serialVersionUID = 1L;
-	
 	public ApiException(String string) {
 		super(string);
 	}
